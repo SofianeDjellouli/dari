bc9eaa2fa52767b68d271bd1241c003fbe127dd0
sofianedjellouli <sof.djell@gmail.com>
2021-11-29 08:00:16 +0100

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,23 @@
 module.exports = {
   extends: ["blitz"],
+  rules: {
+    "padding-line-between-statements": [
+      "error",
+      {
+        blankLine: "always",
+        next: "*",
+        prev: "*",
+      },
+      {
+        blankLine: "never",
+        next: "import",
+        prev: "*",
+      },
+      {
+        blankLine: "any",
+        next: ["case", "export"],
+        prev: ["case", "export"],
+      },
+    ],
+  },
 }
