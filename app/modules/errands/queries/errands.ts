a4fed65be20fe2864ef8aa16f5801f549541bb80
sofianedjellouli <sof.djell@gmail.com>
2021-12-15 10:43:02 +0100

    Update getErrand

--- app/modules/errands/queries/errands.ts
+++ app/modules/errands/queries/errands.ts
@@ -1,12 +1,20 @@
 import { Ctx } from "blitz"
 import db from "db"
 
-export default async function getErrand(_: null, ctx: Ctx) {
+export default async function getErrand(idCursor: number | null, ctx: Ctx) {
   ctx.session.$authorize()
 
-  const errand = await db.errand.findFirst({
-    where: { id: 1 },
-    select: { id: true, name: true, level: true },
+  const errand = await db.errand.findMany({
+    ...(idCursor && {
+      cursor: { id: idCursor },
+      skip: 1,
+    }),
+    take: 10,
+    select: {
+      id: true,
+      name: true,
+      level: true,
+    },
   })
 
   return errand
