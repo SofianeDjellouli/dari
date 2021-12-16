32fe7c8b303c2c0e32d7cb9a2f11504d95513819
sofianedjellouli <sof.djell@gmail.com>
2021-12-16 17:24:53 +0100

    Seed db

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,4 +1,4 @@
-// import db from "./index"
+import db from "./index"
 
 /*
  * This seed function is executed when you run `blitz db seed`.
@@ -7,10 +7,18 @@
  * or https://github.com/Marak/Faker.js to easily generate
  * realistic data.
  */
-const seed = async () => {
-  // for (let i = 0; i < 5; i++) {
-  //   await db.project.create({ data: { name: "Project " + i } })
-  // }
-}
+const seed = async () =>
+  (
+    [
+      { level: "Present", name: "jazar" },
+      { level: "Present", name: "mouz" },
+      { level: "Missing", name: "dajaj" },
+      { level: "Missing", name: "joubn" },
+      { level: "Lacking", name: "halib" },
+      { level: "Present", name: "zit" },
+    ] as const
+  ).forEach(async (data, i) => {
+    await db.errand.create({ data })
+  })
 
 export default seed
