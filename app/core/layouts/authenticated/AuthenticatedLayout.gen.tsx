06201ef49a3b081ad05a5f1d48f31990d64313eb
sofianedjellouli <sof.djell@gmail.com>
2021-12-20 10:31:41 +0100

    Add useToggle

--- app/core/layouts/authenticated/AuthenticatedLayout.gen.tsx
+++ app/core/layouts/authenticated/AuthenticatedLayout.gen.tsx
@@ -2,9 +2,9 @@
 /* eslint-disable import/first */
 
 import * as React from "react"
+
 // @ts-ignore: Implicit any on import
 import * as AuthenticatedLayoutBS__Es6Import from "./AuthenticatedLayout.bs"
-
 const AuthenticatedLayoutBS: any = AuthenticatedLayoutBS__Es6Import
 
 // tslint:disable-next-line:interface-over-type-literal
--- app/core/layouts/unauthenticated/UnauthenticatedLayout.gen.tsx
+++ app/core/layouts/unauthenticated/UnauthenticatedLayout.gen.tsx
@@ -2,9 +2,9 @@
 /* eslint-disable import/first */
 
 import * as React from "react"
+
 // @ts-ignore: Implicit any on import
 import * as UnauthenticatedLayoutBS__Es6Import from "./UnauthenticatedLayout.bs"
-
 const UnauthenticatedLayoutBS: any = UnauthenticatedLayoutBS__Es6Import
 
 // tslint:disable-next-line:interface-over-type-literal
